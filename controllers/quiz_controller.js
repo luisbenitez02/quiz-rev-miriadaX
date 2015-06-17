4dd520ec3cb2a14d0180efa2371da0fd7154aacc
Luis Benitez <luisbenitez02@gmail.com>
2015-06-16 20:12:37 -0500

    Actualizado a prototipo 1
    La app se actualizo al prototipo 1 hasta el momento desarrollado en el
    curso MiriadaX

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,14 @@
+// GET /quizes/question
+exports.question = function(req,res){
+	res.render('quizes/question', {pregunta: '¿Cual es la capital de Italia?'});
+};
+
+exports.answer = function(req, res){
+	if (req.query.respuesta === 'Roma') {
+		res.render('quizes/answer', {respuesta: 'Correcto'});
+	} else {
+		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+	}
+};
+/*esa variable pregunta se la estamos enviando a las vistas en la
+carpeta quizes dentro de views*/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+//Pasos 1b y 1c Importamos el enrutador
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });//titulo de la pagina
 });
 
+//Definimos dos rutas para el acceso
+router.get('/quizes/question', quizController.question);//esto va a estar conectado con las vistas
+//por ello se creo la carpeta quizes y se van a crear unos index muy cools
+router.get('/quizes/answer', quizController.answer);
 module.exports = router;
